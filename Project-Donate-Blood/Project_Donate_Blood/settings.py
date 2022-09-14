2013fc5355d462e5879635ea6dc4295e2019d577
FouhChainz <zhaco98@gmail.com>
2022-09-14 19:15:27 +0200

    Edit line

--- Project-Donate-Blood/Project_Donate_Blood/settings.py
+++ Project-Donate-Blood/Project_Donate_Blood/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cc794a09-d390-4ec0-a6ed-df9ccf360546'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
