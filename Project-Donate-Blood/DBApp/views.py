b07f492534655db1c94b6c4290bb215c55bd09de
Linus Magnusson <panini_94@hotmail.com>
2022-10-03 14:28:29 +0200

    first round of making a list

--- Project-Donate-Blood/DBApp/views.py
+++ Project-Donate-Blood/DBApp/views.py
@@ -5,6 +5,9 @@ from .forms import *
 import pandas as pd
 import folium
 import geocoder
+from geopy.distance import geodesic
+import operator
+import numpy as np
 
 #This shows what you want the index page to show.
 def index(request):
@@ -45,15 +48,37 @@ def test1(request):
     else:
         form = SearchForm()
     address = Search.objects.all().last()
-    location = geocoder.osm(address)
+    location = geocoder.osm(str(address) + ', ' + 'gothenburg')
     
     lat = location.lat
     lng = location.lng
-    country = 'Sweden'
+    coordinates = (lat,lng)
+
     if lat == None or lng == None:
         address.delete()
         return HttpResponse('You address input is invalid')
 
+    distance = []
+    for i in range(len(df)):
+          distance.append([i, geodesic(coordinates, (float(df['Latitude'][i]), float(df['Longitude'][i]))).km])
+
+    distance_sorted = distance
+    distance_sorted = sorted(distance_sorted, key=operator.itemgetter(1))
+    #sorted by distance in array with index, distance
+    print(distance_sorted)
+    test = [item[0] for item in distance_sorted]
+    #just the indexes
+    print(test)
+
+    test_take = test[0:5]
+    #closest 5 indexes
+    print(test_take)
+
+    df_2 = df.iloc[df.index.isin(test_take)]
+    #new df with info from original df of the 5 closest places
+    print(df_2)
+    #convert df to html so i can show it on the webpage
+    df_2html = df_2.to_html()
     # Create Map Object
     map = folium.Map(location=[57.708870, 11.974560], zoom_start = 11)
 
@@ -74,5 +99,6 @@ def test1(request):
     context = {
         'map': map,
         'form': form,
+        'list': df_2html,
     }
     return render(request, 'DBApp/test.html', context)
